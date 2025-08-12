3e655469b5da40f4ca297b4ff2f99c048d8034a3
senn912 <truongtrongsang2003@gmail.com>
2025-08-13 00:19:55 +0700

    refactor structure

--- react+vite/src/App.jsx
+++ react+vite/src/App.jsx
@@ -1,30 +1,13 @@
-import Button from '@mui/material/Button'
-import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
-import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
-import HomeIcon from '@mui/icons-material/Home'
-import { pink } from '@mui/material/colors';
+import Board from './pages/Boards/_id'
+
 function App() {
 
   return (
     <>
-      <div>Sang banh trai</div>
-      <Button variant="contained" >Hello world</Button>
-      <Button variant="text">Text</Button>
-      <Button variant="contained">Contained</Button>
-      <Button variant="outlined">Outlined</Button>
-      <br />
-
-      <AccessAlarmIcon />
-      <ThreeDRotation />
-      <HomeIcon color="primary" />
-      <HomeIcon color="secondary" />
-      <HomeIcon color="success" />
-      <HomeIcon color="action" />
-      <HomeIcon color="disabled" />
-      <HomeIcon sx={{ color: pink[200] }} />
-
+    <Board/>
     </>
   )
 }
+
 
 export default App
---
+++ react+vite/src/utils/sorts.js
@@ -0,0 +1,1 @@
+//sort collumns/cards
